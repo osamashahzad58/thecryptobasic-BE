5128228307a07343f8b8d99ecddefc152c36c06d
muhammadwaqas4 <mwaqasbashir4@gmail.com>
2025-08-27 16:57:56 +0500

    evm and sol Wallet's data

---
+++ src/web3/evm.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const API_KEY = process.env.API_KEY_MORALIS;
+
+const CHAIN = "bsc"; // change chain accordingly e.g "eth", "bsc", "polygon", "avalanche", "fantom", etc.
+
+async function getWalletTokens(walletaddress, chain) {
+
+    const url = `https://deep-index.moralis.io/api/v2.2/wallets/${walletaddress}/tokens?chain=${chain}`;
+
+    const res = await fetch(url, {
+      headers: {
+        "accept": "application/json",
+        "X-API-Key": API_KEY,
+      },
+    });
+
+    const tokens = await res.json();
+  
+    tokens.result.forEach((t) => {
+        console.log({
+            tokenAddress: t.token_address,
+            name: t.name,
+            symbol: t.symbol,
+            balance: Number(t.balance) / (10 ** t.decimals),
+            priceUSD: t.usd_price || 0,
+            totalValueUSD: (Number(t.balance) / (10 ** t.decimals)) * (t.usd_price || 0),
+        });
+    });
+
+    return ({
+        tokenAddress: t.token_address,
+        name: t.name,
+        symbol: t.symbol,
+        balance: Number(t.balance) / (10 ** t.decimals),
+        priceUSD: t.usd_price || 0,
+        totalValueUSD: (Number(t.balance) / (10 ** t.decimals)) * (t.usd_price || 0),
+    });
+}
